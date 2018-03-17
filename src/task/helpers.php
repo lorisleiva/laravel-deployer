d6a2cbdc9dcf74524a4d0dc59aaa03f1a4cafd44
Loris LEIVA <loris.leiva@gmail.com>
2018-03-17 14:27:04 +0100

    :ambulance: Fix helper function artisan

--- src/task/helpers.php
+++ src/task/helpers.php
@@ -7,7 +7,7 @@ namespace Deployer;
  */
 
 function artisan($command, $showOutput = false) {
-    return function() use ($showOutput) {
+    return function() use ($command, $showOutput) {
         $output = run("{{bin/php}} {{release_path}}/artisan $command");
         
         if ($showOutput) {
