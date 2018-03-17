58e9012388efbe89749f3a993b901d02c3772a80
Loris LEIVA <loris.leiva@gmail.com>
2018-03-17 14:40:45 +0100

    :pencil: Use npm binaries

--- src/task/npm.php
+++ src/task/npm.php
@@ -5,7 +5,7 @@ namespace Deployer;
 require 'recipe/npm.php';
 
 desc('Execute npm run development');
-task('npm:development', 'npm run development');
+task('npm:development', '{{bin/npm}} run development');
 
 desc('Execute npm run production');
-task('npm:production', 'npm run production');
+task('npm:production', '{{bin/npm}} run production');
