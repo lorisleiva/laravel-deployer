5e4193a27a3f18c64bdab350438e650f3793e088
Loris LEIVA <loris.leiva@gmail.com>
2018-03-24 11:49:10 +0100

    :pencil: Draft local deploy strategy

---
+++ src/task/local.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Deployer;
+
+set('local_deploy_path', __DIR__ . '/../../.build');
+set('local_upload_options', [
+    'exclude' => [ 'node_modules' ],
+]);
+
+desc('Build your application locally');
+task('local:build', function() {
+    set('deploy_path', get('local_deploy_path'));
+    invoke('deploy:prepare');
+    invoke('deploy:release');
+    invoke('deploy:update_code');
+    invoke('deploy:vendors');
+    invoke('deploy:symlink');
+})->local();
+
+desc('Upload your locally-built application to your hosts');
+task('local:upload', function () {
+    upload(
+        '{{local_deploy_path}}/current/',   // From local path
+        '{{release_path}}',                 // To server release
+        get('local_upload_options')         // With upload options
+    );
+});
+
+desc('Remove locally-built application');
+task('local:cleanup', function () {
+    run('rm -rf {{local_deploy_path}}');
+})->local();
